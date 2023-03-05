190aa6642aa153c340f8cd8ced0608e1e4292791
KHOA <khoa01675065117@gmail.com>
2023-03-05 16:11:49 +0700

    add login and logout page

--- frontend/my_project/src/App.js
+++ frontend/my_project/src/App.js
@@ -1,34 +1,61 @@
 import './App.css';
 import { useState, useEffect } from 'react'
 import axios from "axios"
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 function App() {
-  const [customers, setCustomers] =  useState([])
-  useEffect(() => {
-    async function getAllCustomers(){
-      try {
-        const customers = await axios.get("http://127.0.0.1:8000/cellphone/customer/")
-        console.log(customers.data)
-        setCustomers(customers.data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    getAllCustomers()
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Connect ReactJS to Django</h1>
-        {
-          customers.map((customer, i)=>{
-            return (
-              <h2>{customer.id} {customer.full_name}</h2>
-            )
-          })
-        }
-      </header>
-    </div>
+    <AppContainer>
+      <AccountBox />
+    </AppContainer>
   );
 }
+
+
+
+
+
+
+
+
+// function App() {
+//   const [customers, setCustomers] =  useState([])
+//   useEffect(() => {
+//     async function getAllCustomers(){
+//       try {
+//         const customers = await axios.get("http://127.0.0.1:8000/cellphone/customer/")
+//         console.log(customers.data)
+//         setCustomers(customers.data)
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     }
+//     getAllCustomers()
+//   }, [])
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Connect ReactJS to Django</h1>
+//         {
+//           customers.map((customer, i)=>{
+//             return (
+//               <h2>{customer.id} {customer.full_name}</h2>
+//             )
+//           })
+//         }
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
